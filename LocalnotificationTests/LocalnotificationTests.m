5cb838b4e3b4dca404e7ccd2100e604046b854ce
quyunhaoai <quyunhaoai@163.com>
2018-02-28 10:25:08 +0800

    ,

---
+++ LocalnotificationTests/LocalnotificationTests.m
@@ -0,0 +1,40 @@
+//
+//  LocalnotificationTests.m
+//  LocalnotificationTests
+//
+//  Created by 王栋栋 on 15-5-14.
+//  Copyright (c) 2015年 王栋栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LocalnotificationTests : XCTestCase
+
+@end
+
+@implementation LocalnotificationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
